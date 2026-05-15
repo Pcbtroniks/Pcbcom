292fa5058f6eca5c8434d8168b7f07fcadb14c20
Brandoxo <brandoxo90@gmail.com>
2026-05-15 16:10:28 -0600

    ADD\ Product components

---
+++ resources/js/Components/Skeletons/skeleton-card-product.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="col-span-1 rounded-xl py-4">
+            <article class="overflow-hidden rounded-2xl shadow-lg animate-pulse">
+                <div class="bg-gray-200 h-48 w-96"></div>
+                <div class="p-4">
+                    <div class="bg-gray-200 h-4 w-16 rounded-md"></div>
+                    <div class="mt-2 mb-2 flex flex-col gap-2 border-t border-gray-200 pt-2">
+                        <div class="bg-gray-200 h-3 w-20 rounded"></div>
+                        <div class="bg-gray-200 h-4 w-full rounded"></div>
+                        <div class="bg-gray-200 h-4 w-3/4 rounded"></div>
+                        <div class="bg-gray-200 h-3 w-1/2 rounded"></div>
+                    </div>
+                    <div class="flex mt-4 gap-2 items-center">
+                        <div class="bg-gray-200 h-7 w-20 rounded"></div>
+                        <div class="bg-gray-200 h-5 w-10 rounded"></div>
+                    </div>
+                </div>
+                <div class="w-full bg-gray-200 h-10 rounded-b"></div>
+            </article>
+        </div>
+    </div>
+</template>
--- resources/js/Components/card-product.vue
+++ resources/js/Components/card-product.vue
@@ -6,27 +6,31 @@
 
 <template>  
     <div>
-        <div  data-aos="zoom-in" data-aos-delay="100" class="col-span-1 rounded-xl">
+        <div data-aos="zoom-in" data-aos-delay="100" class="col-span-1 rounded-xl py-4">
           <article class="overflow-hidden rounded-2xl shadow-lg">
-            <div>            
-                <img :src="product.imagen || product.image" :alt="product.nombre || product.title" class="bg-cover mx-auto">
+            <div class="bg-gray-200 h-64 w-full relative">
+              <span :class="`px-2 text-center bg-gray-100 uppercase text-xs font-semibold rounded-md absolute top-4 left-2 ${product.categorias[0]?.nombre ? 'block' : 'hidden'}`">
+                {{ product.categorias[0]?.nombre || product.category }}
+              </span>          
+              <img :src="product.img_portada || product.image" :alt="product.nombre || product.title" class="bg-cover mx-auto h-64 w-full object-cover">
             </div>
             <div class="p-4">
-              <span :class="`px-2 text-center bg-gray-100 text-green-600 text-xs font-semibold rounded-md `">
-                {{ product.etiqueta }}
-              </span>
-              <div class="mt-2 mb-2">
-              <h5 class="font-bold text-2xl">{{ product.title || product.nombre }}</h5>
-              <small class="text-muted text-gray-500">{{ product.category  || product.categoria }}</small>
+
+              <div class="mb-2 flex flex-col gap-1 border-t border-gray-200 pt-2">
+              <p class="text-green-600 font-extrabold text-xs">{{ product.marca || product.brand }}</p>
+              <a href="product.url" class="text-md text-blue-600 hover:text-blue-800 hover:underline">
+                <h5 class="font-bold text-md text-blue-600 hover:text-blue-800 hover:underline line-clamp-3 ">{{ product.titulo || product.title }}</h5>
+              </a>
+              <small class="text-gray-500">{{ product.modelo  || product.model }}</small>
               </div>
-              <p class="text-gray-600 ">{{ product.description || product.descripcion }}</p>
-              <div class="flex items-center justify-between mt-4">
-                <strong class="text-md font-bold text-green-600">${{ product.price || product.precio }}</strong>
-                <div class="bg-white">
-                <a href="" class="text-sm border p-1 border-green-600 text-green-600 hover:bg-green-600 hover:text-white">Agregar</a>
-                </div>
+              <div class="flex font-bold mt-4 gap-2">
+                <strong class="text-xl font-bold">${{ product.precios.precio_1 || product.precio }}</strong>
+                <span class="text-lg">USD</span>
               </div>
-              </div>
+            </div>
+            <div class="w-full flex items-center justify-center mt-2 bg-green-600 text-white rounded-b p-2">
+            <a href="" class="">Agregar</a>
+            </div>
             </article>
           </div>
       </div>
